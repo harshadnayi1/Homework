caf3186322f59e5685379179f7bb36fdeac05f24
harshadnayi1 <102856237+harshadnayi1@users.noreply.github.com>
2022-04-08 19:11:47 +0100

    Update App.java

--- App.java
+++ App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Git Hub Homework " );
+        System.out.println( "git pull --rebase <remote> " );
     }
 }
